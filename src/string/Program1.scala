8bc4f0c5517b91b73c7eae83d4dd4df70f896a07
RAVI VERMA <ravi.verma278@gmail.com>
2019-10-22 20:58:25 +0530

    Update Program1.scala

--- src/string/Program1.scala
+++ src/string/Program1.scala
@@ -9,8 +9,14 @@ object Program1 {
     var z=Array("Ram","raVi","raJ")
     
     for(elm <- z){
-      println(elm.map(_.toLower).capitalize);
-      val small=elm.charAt(0)
+      //1St Way
+      //println(elm.map(_.toLower).capitalize);
+      //One more way to do it
+      var newel=elm.map(_.toLower)
+      val chars = newel.toCharArray
+      var l2=chars(0).toUpper
+      var newlist=newel.toList
+      println((l2::newlist.drop(1))mkString(""))
       //println(elm.capitalize)
       
       
